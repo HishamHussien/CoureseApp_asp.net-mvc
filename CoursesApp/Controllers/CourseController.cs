7a224ff9182d69663959801c60e36b834202bb83
Hesham <hishamtecno@gmail.com>
2022-10-01 16:47:10 +0200

    init course App

---
+++ CoursesApp/Controllers/CourseController.cs
@@ -0,0 +1,34 @@
+﻿using CoursesApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CoursesApp.Controllers
+{
+    public class CourseController : Controller
+    {
+        // GET: Course
+        public ActionResult Index()
+        {
+            var lstcourses = new List<CourseModel>
+        {
+            new CourseModel
+            {
+                Id=1,
+                Tilte="xamarian c# ",
+                Description="This description for xamarin course 111"
+            },
+
+            new CourseModel
+            {
+                Id=1,
+                Tilte="xamarian c# 2 ",
+                Description="This description for xamarin course 222"
+            }
+        };
+            return View(lstcourses);
+        }
+    }
+}
---
+++ CoursesApp/Models/CourseModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CoursesApp.Models
+{
+    public class CourseModel
+    {
+        public int Id { get; set; }
+        public string Tilte { get; set; }
+        public string Description { get; set; }
+        public int categoryId { get; set; }
+        public CategoryModel Category { get; set; }
+    }
+}
